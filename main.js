3857f50d823e5c851e6dc4f29a415ed52425c33e
psybo2g <psybo2g@gmail.com>
2026-01-30 13:17:26 +0530

    Remove copyright comments and clean up code
    Removed copyright header comments and updated function structure.

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-/*
- * Project Name: schulte-table
- * Copyright (C) 2026  psybo2g
- * * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-// Using more reliable Fisher-Yates Shuffle
 function generateRandomNumbers(size) {
     const numbers = Array.from({ length: size }, (_, i) => i + 1);
     // Fisher-Yates shuffle
